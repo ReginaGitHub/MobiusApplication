1296cc6f41539ce94c9723ff2a9483e92f9ad8d4
ReginaGitHub <reginaang76_@hotmail.com>
2021-03-13 16:51:01 +0800

    First commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // MongoURI: 'mongodb+srv://mobiusnyp:mobius123@cluster0.cttlu.mongodb.net/test?retryWrites=true&w=majority'
+    MongoURI: 'mongodb+srv://regina123:regina123@test-cluster1.wfn7z.mongodb.net/MobiusApplication?retryWrites=true&w=majority'
+}
