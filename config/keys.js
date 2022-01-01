cc5e5a9a3dfda54e51591bd0d5007f2bc6ac7d10
ReginaGitHub <61978015+ReginaGitHub@users.noreply.github.com>
2022-01-01 23:10:20 +0800

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
     // MongoURI: 'mongodb+srv://mobiusnyp:mobius123@cluster0.cttlu.mongodb.net/test?retryWrites=true&w=majority'
-    MongoURI: 'mongodb+srv://regina123:regina123@test-cluster1.wfn7z.mongodb.net/MobiusApplication?retryWrites=true&w=majority'
+    MongoURI: 'mongodb+srv://YOUR_USERNAME_HERE:YOUR_PASSWORD_HERE@CLUSTER_NAME_HERE.mongodb.net/MobiusApplication?retryWrites=true&w=majority'
 }
